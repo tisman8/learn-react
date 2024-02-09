653fb10ee8e40b9d62df5e9bf5acbeb915ec5c38
Tracy Isman <tracyisman@gmail.com>
2024-02-09 18:06:35 -0500

    first component quiz done

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Profile from './components/profile'
+import Profile from './qcomps/profile_mistake'
+import MyComp from './qcomps/firstcomp'
 
 export default function Home() {
   return (
     <div className={styles.main}>
         <Profile />
+        <MyComp />
     </div>
   )
 }
